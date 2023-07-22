9bfbbffe9dcdf929097ba69e598b5c6ef625d357
Arvind <thakurarvindkr10@gmail.com>
2023-07-22 22:59:56 +0530

    added products

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Products.css'
+import Product from './Product'
 const Products = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <section className="card-container">
+                <Product/>
+            </section>
+        </>
     )
 }
 
-export default Products
+export default Products;
