f290c49d601d31f45d6b05f19fb09235308bb098
Arvind <thakurarvindkr10@gmail.com>
2023-07-23 16:10:27 +0530

    Price done

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Price.css'
+
 const Price = () => {
     return (
-        <div>
-            
+        <div className='ml'>
+            <h2 className="sidebar-title price-title">Price</h2>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test2" />
+                <span className='checkMark'></span>All
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test2" />
+                <span className='checkMark'></span>$0-$50
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test2" />
+                <span className='checkMark'></span>$50-$100
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test2" />
+                <span className='checkMark'></span>$150-$200
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test2" />
+                <span className='checkMark'></span>over $500
+            </label>
         </div>
     )
 }
